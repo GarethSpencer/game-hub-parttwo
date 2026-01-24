3f68662cf27c7f6224cd447def150700d05b9c12
Gareth Spencer <gareth.spencer1@hotmail.co.uk>
2026-01-24 20:32:41 +0000

    Initial commit

---
+++ src/hooks/usePlatforms.ts
@@ -0,0 +1,22 @@
+import platforms from "../data/platforms";
+import apiClient, { FetchResponse } from "../services/api-client";
+import { useQuery } from "@tanstack/react-query";
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1001, //24h
+    initialData: { count: platforms.length, results: platforms },
+  });
+
+export default usePlatforms;
---
+++ src/services/api-client.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
+export default axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "d14aeb6996974576bbd4b06c1bf9318e",
+  },
+});
