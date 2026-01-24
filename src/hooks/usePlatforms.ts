d1b93f17f571ac1019fde4d66fbecebdafc4dcec
Gareth Spencer <gareth.spencer1@hotmail.co.uk>
2026-01-24 20:42:51 +0000

    fixing github repo

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -15,7 +15,7 @@ const usePlatforms = () =>
       apiClient
         .get<FetchResponse<Platform>>("/platforms/lists/parents")
         .then((res) => res.data),
-    staleTime: 24 * 60 * 60 * 1001, //24h
+    staleTime: 24 * 60 * 60 * 1000, //24h
     initialData: { count: platforms.length, results: platforms },
   });
 
