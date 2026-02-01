6e949e7f744fe779f7a472a916df9a5ceab5449d
Gareth Spencer <gareth.spencer1@hotmail.co.uk>
2026-02-01 16:01:17 +0000

    Reset gamequery when the logo is clicked

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,20 @@
 import { HStack, Image } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.webp";
+import useGameQueryStore from "../store";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
 const NavBar = () => {
+  const reset = useGameQueryStore((s) => s.reset);
   return (
     <HStack padding="10px">
-      <Link to={"/"}>
+      <Link
+        to={"/"}
+        onClick={() => {
+          reset();
+        }}
+      >
         <Image src={logo} boxSize="60px" objectFit={"cover"} />
       </Link>
       <SearchInput />
